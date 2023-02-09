d280862361e586d791fc23f336c3ca6a656dcd45
izzetemredemir <izzetemredemir@gmail.com>
2023-02-10 01:49:03 +0300

    routes/urls.js :: yardim/:id yardimKaydi add

--- routes/urls.js
+++ routes/urls.js
@@ -494,16 +494,19 @@ router.get("/yardim/:id", async (req, res) => {
         return yedekTelefon.replace(/.(?=.{4})/g, "*");
       });
     }
+  
     let yardimKaydi = await YardimKaydi.find({postId:req.params.id});
-    
-    results["yardimKaydi"] = yardimKaydi;
-
-
+  
     cache.getCache().set(cacheKey, results);
     if (!results) {
       return res.status(404).send("Yardim not found");
     }
-    res.send(results);
+    if(!data){
+      res.send({
+        results: results,
+        yardimKaydi: yardimKaydi
+      });
+    }
   } catch (error) {
     console.error(error);
     res.status(500).send("Error occurred while fetching Yardim");
