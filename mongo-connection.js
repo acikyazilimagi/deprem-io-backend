1b6824f34442a397180f30a23d3b9e7b1f208373
Nazmi Yılmaz <nazmiyilmaz.gml@gmail.com>
2023-02-10 15:30:13 +0300

    fix export yardimet

--- mongo-connection.js
+++ mongo-connection.js
@@ -1,17 +1,17 @@
-require('dotenv').config();
-const mongoUrl = process.env.MONGOURL;
-console.log("mongoUrl",mongoUrl);
+require('dotenv').config()
+const mongoUrl = process.env.MONGOURL
 
-const mongoose = require('mongoose');
-    const connectDB = async () => {
-        try {
-            await mongoose.connect(mongoUrl);
-    
-            console.log('MongoDB connected!!');
-        } catch (err) {
-            console.log('Failed to connect to MongoDB', err);
-        }
-    };
-    
+console.log('mongoUrl', mongoUrl)
 
-module.exports = connectDB;
+const mongoose = require('mongoose')
+const connectDB = async () => {
+   try {
+      await mongoose.connect(mongoUrl)
+
+      console.log('MongoDB connected!!')
+   } catch (err) {
+      console.log('Failed to connect to MongoDB', err)
+   }
+}
+
+module.exports = connectDB
--- scripts/exportYardimEtCsv.js
+++ scripts/exportYardimEtCsv.js
@@ -1,44 +1,65 @@
-const mongoose = require("mongoose");
-const YardimEt = require("../models/yardimEtModel");
-const fs = require("fs");
-const connectDB = require("../mongo-connection");
+const mongoose = require('mongoose')
+const YardimEt = require('../models/yardimEtModel')
+const connectDB = require('../mongo-connection')
+
+const ObjectsToCsv = require('objects-to-csv')
 
 // Datayı csv olarak export etmek için script
 // node scripts/exportYardimEtCsv.js
 // ekstra fields kımsında bug var
 
-
 async function main() {
-await checkConnection();
-const writeStream = fs.createWriteStream("yardimEt.csv");
+   await checkConnection()
 
-await YardimEt.find({})
-  .select("yardimTipi adSoyad telefon sehir hedefSehir aciklama fields ip createdAt updatedAt")
-  .then(docs => {
-    writeStream.write("yardimTipi,adSoyad,telefon,sehir,hedefSehir,aciklama,fields,createdAt,updatedAt\n");
-    docs.forEach(doc => {
-      writeStream.write(
-        `"${doc.yardimTipi}","${doc.adSoyad}","${doc.telefon}","${doc.sehir}","${doc.hedefSehir}","${doc.aciklama}","${JSON.stringify(
-          doc.fields
-        )}","${doc.createdAt}","${doc.updatedAt}"\n`
-      );
-    });
-    writeStream.end();
-    console.log("CSV file exported successfully.");
-   mongoose.connection.close();
-  })
-  .catch(err => {
-    console.error(err);
-    mongoose.connection.close();
-  });
+   const select = [
+      'ip',
+      'yardimTipi',
+      'adSoyad',
+      'telefon',
+      'sehir',
+      'hedefSehir',
+      'aciklama',
+      'createdAt',
+      'updatedAt',
+      'fields',
+   ]
 
+   try {
+      const docs = await YardimEt.find({}).select(select).lean()
+
+      const flat = docs.map(function (doc) {
+         const hold = {}
+
+         for (const key of select) {
+            // eğer nested yapıdaysa
+            if (typeof doc[key] === 'object') {
+               for (const subKey of Object.keys(doc[key])) {
+                  hold['ekstra_' + subKey] = doc[key][subKey]
+               }
+            }
+            // eğer nested değilse
+            else hold[key] = doc[key]
+         }
+
+         return hold
+      })
+
+      const csv = new ObjectsToCsv(flat)
+      const file = ['Yardım_Et_Export_' + Date.now(), 'csv'].join('.')
+      await csv.toDisk(`./${file}`, { allColumns: true })
+      console.log('Yardım verileri dışarı aktarıldı')
+      mongoose.connection.close()
+   } catch (error) {
+      console.log(error)
+      mongoose.connection.close()
+   }
 }
 
-  async function checkConnection() {
-    if (mongoose.connection.readyState !== 1) {
-        console.log("burası");
-      await connectDB();
-    }
-  }
-  
-  main();
+async function checkConnection() {
+   if (mongoose.connection.readyState !== 1) {
+      console.log('Veritabanı bağlantısı sağlandı')
+      await connectDB()
+   }
+}
+
+main()
