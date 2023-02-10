2b8d5d4669e46646623eaf19d4cef0124d0aaa7f
kadirkurhan <kadirkurhan42@hotmail.com>
2023-02-10 09:25:18 +0300

    feat: row count added to collaboration response

--- Controller/Collabration/CollabrationController.js
+++ Controller/Collabration/CollabrationController.js
@@ -1,73 +1,77 @@
-const CollabrationListing = require('../../models/Collobration/CollabrationListing');
+const CollabrationListing = require("../../models/Collobration/CollabrationListing");
 
 class CollabrationController {
-    async getAll(req, res) {
-        try {
-          const query = req.query;
-          let where = {};
-          if (query.city) {
-            where.City = query.city;
-          }
-          if (query.district) {
-            where.District = query.district;
-          }
-          if (query.type) {
-            where.needType = { [Op.in]: query.type.split(',') };
-          }
-          if (query.emergency) {
-            where.emergencyStatus = query.emergency;
-          }
-          const collabrationListings = await CollabrationListing.find({
-            where,
-          });
-          res.json({
-            totalPage:0,
-            data:collabrationListings
-
-          });
-        } catch (error) {
-            res.status(500).json({
-                message: error.message,
-            });
-        }
-    }
-    async show(req, res) {
-      try {
-        const collabrationListing = await CollabrationListing.findById(req.params.id)
-        return res.json({
-          totalPage:0,
-          data:collabrationListing,
-
-        });
-      } catch (error) {
-          res.status(500).json({
-              message: error.message,
-          });
+  async getAll(req, res) {
+    try {
+      const query = req.query;
+      let where = {};
+      if (query.city) {
+        where.City = query.city;
       }
+      if (query.district) {
+        where.District = query.district;
+      }
+      if (query.type) {
+        where.needType = { [Op.in]: query.type.split(",") };
+      }
+      if (query.emergency) {
+        where.emergencyStatus = query.emergency;
+      }
+      const collabrationListings = await CollabrationListing.find({
+        where,
+      });
+      res.json({
+        totalPage: 0,
+        rowCount: CollabrationListing.countDocuments(where),
+        data: collabrationListings,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
     }
-
-    async store(req, res) {
-        const collabrationListing = await CollabrationListing.create(req.body);
-
-        return res.json(collabrationListing);
+  }
+  async show(req, res) {
+    try {
+      const collabrationListing = await CollabrationListing.findById(
+        req.params.id
+      );
+      return res.json({
+        totalPage: 0,
+        data: collabrationListing,
+      });
+    } catch (error) {
+      res.status(500).json({
+        message: error.message,
+      });
     }
+  }
 
-    async update(req, res) {
-        const collabrationListing = await CollabrationListing.findById(req.params.id);
+  async store(req, res) {
+    const collabrationListing = await CollabrationListing.create(req.body);
 
-        await collabrationListing.update(req.body);
+    return res.json(collabrationListing);
+  }
 
-        return res.json(collabrationListing);
-    }
+  async update(req, res) {
+    const collabrationListing = await CollabrationListing.findById(
+      req.params.id
+    );
 
-    async delete(req, res) {
-        const collabrationListing = await CollabrationListing.findById(req.params.id);
+    await collabrationListing.update(req.body);
 
-        await collabrationListing.destroy();
+    return res.json(collabrationListing);
+  }
 
-        return res.send();
-    }
+  async delete(req, res) {
+    const collabrationListing = await CollabrationListing.findById(
+      req.params.id
+    );
+
+    await collabrationListing.destroy();
+
+    return res.send();
+  }
 }
 
 module.exports = new CollabrationController();
-
