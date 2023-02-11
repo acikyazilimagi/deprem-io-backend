efbd066182bf77579ea77b58d58d381c01108750
Mehmet Karabulut <mehmet@mingapp.co>
2023-02-11 20:27:20 +0300

    saat başı stklara otomatik mail gönderim özelliği mailgun kutuphanesi eklendi.

--- services/yardimMails.js
+++ services/yardimMails.js
@@ -1,10 +1,11 @@
-const mailService = require('./mail');
-const Yardim = require('../models/yardimModel');
+const mailService = require("./mail");
+const Yardim = require("../models/yardimModel");
 async function sendYardimList(yardimTipi) {
   const yardimlar = await Yardim.find({ yardimTipi: yardimTipi, gonderildi: false }).exec();
   const yardimSayisi = yardimlar.length;
   if (yardimSayisi >= 10) {
-    let html = '<table border="1"><thead><tr><th>Yardım Tipi</th><th>Ad Soyad</th><th>Telefon</th><th>Yedek Telefonlar</th><th>Email</th><th>Adres</th><th>Adres Tarifi</th><th>Acil Durum</th><th>Kişi Sayısı</th><th>Yardım Durumu</th><th>Fiziki Durum</th><th>Google Map Link</th><th>Tweet Link</th><th>Fields</th><th>IP</th><th>Gönderildi</th></tr></thead><tbody>';
+    let html =
+      '<table border="1"><thead><tr><th>Yardım Tipi</th><th>Ad Soyad</th><th>Telefon</th><th>Yedek Telefonlar</th><th>Email</th><th>Adres</th><th>Adres Tarifi</th><th>Acil Durum</th><th>Kişi Sayısı</th><th>Yardım Durumu</th><th>Fiziki Durum</th><th>Google Map Link</th><th>Tweet Link</th><th>Fields</th><th>IP</th><th>Gönderildi</th></tr></thead><tbody>';
 
     for (let yardim of yardimlar) {
       yardim.fields = JSON.stringify(yardim.fields);
@@ -14,12 +15,12 @@ async function sendYardimList(yardimTipi) {
       await yardim.save();
     }
 
-    html += '</tbody></table>';
+    html += "</tbody></table>";
 
     const to = process.env.MAIL_RECEIVERS;
-    const str = process.env.MAIL_SUBJECT + ' - ' + yardimTipi.toUpperCase();
-    let subject = str.replace('%s', yardimSayisi);
-    console.log(yardimSayisi + ' adet ' + yardimTipi + ' yardım talebi gönderildi.');
+    const str = `${process.env.MAIL_SUBJECT} - ${yardimTipi.toUpperCase()}`;
+    let subject = str.replace("%s", yardimSayisi);
+    console.log(`${yardimSayisi} adet ${yardimTipi} yardım talebi gönderildi.`);
     return mailService.sendMail(to, subject, html);
   }
 }
