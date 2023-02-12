a0602b60e4a8b46716a9a156ee97acc18788e964
halilturkoglu <halilturkoglucs@gmail.com>
2023-02-12 01:43:44 +0100

    Static email config.js

--- config.js
+++ config.js
@@ -4,6 +4,6 @@ module.exports = {
   port: parseInt(process.env.PORT, 10) || 8080,
   mongoUrl: process.env.MONGOURL || "",
   NODE_ENV: process.env.NODE_ENV || "development",
-  exportEmails: process.env.EXPORT_EMAILS || "halilturkoglucs@gmail.com",
+  exportEmails: process.env.EXPORT_EMAILS || "istenilenstaticemail@mail.com",
   emailProviderAPIKey: process.env.EMAIL_PROVIDER_API_KEY || "",
 };
