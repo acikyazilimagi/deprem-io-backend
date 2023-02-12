0f9f164803e6a74e1c0bbd1bf69003fb1c9df9a1
izzetemredemir <izzetemredemir@gmail.com>
2023-02-12 16:58:41 +0300

    redisurl fix try

--- config.js
+++ config.js
@@ -10,6 +10,6 @@ if (fs.existsSync(".env")) {
 module.exports = {
   port: parseInt(process.env.PORT, 10) || 8080,
   mongoUrl: process.env.MONGOURL || "",
-  redisUrl: process.env.REDIS_URL || "",
+  redisUrl: process.env.REDIS_URL,
   NODE_ENV: process.env.NODE_ENV || "development",
 };
