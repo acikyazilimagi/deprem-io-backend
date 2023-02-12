3f60e8b20bf8fa42120e254c777cfbfcd94327f6
Okan Pinar <dev.okanpin@gmail.com>
2023-02-12 20:08:47 +0300

    fix: yardimEt controller dan eski cache metodları kalktı

--- routes/controllers/yardimEt.js
+++ routes/controllers/yardimEt.js
@@ -128,9 +128,11 @@ module.exports = async function (fastifyInstance) {
 
       const cacheKey = `yardimet_${page}_${limit}${yardimTipi}${sehir}${hedefSehir}`;
 
-      if (cache.getCache().has(cacheKey)) {
-        return cache.getCache().get(cacheKey);
+      const cacheResult = await fastifyInstance.cache.get(cacheKey);
+      if (cacheResult?.item) {
+        return cacheResult.item;
       }
+
       await checkConnection(fastifyInstance);
 
       if (endIndex < (await YardimEt.countDocuments().exec())) {
@@ -176,7 +178,7 @@ module.exports = async function (fastifyInstance) {
         return yardim;
       });
 
-      cache.getCache().set(cacheKey, results);
+      await fastifyInstance.cache.set(cacheKey, results, 1000 * 60 * 60);
 
       return results;
     },
@@ -185,9 +187,11 @@ module.exports = async function (fastifyInstance) {
   fastifyInstance.get("/yardimet/:id", async (req, res) => {
     const cacheKey = `yardimet_${req.params.id}`;
 
-    if (cache.getCache().has(cacheKey)) {
-      return cache.getCache().get(cacheKey);
+    const cacheResult = await fastifyInstance.cache.get(cacheKey);
+    if (cacheResult?.item) {
+      return cacheResult.item;
     }
+
     await checkConnection(fastifyInstance);
     const results = await YardimEt.findById(req.params.id);
     let yardimKaydi = await YardimKaydi.find({ postId: req.params.id });
@@ -206,10 +210,7 @@ module.exports = async function (fastifyInstance) {
       }
     } catch (error) {}
 
-    cache.getCache().set(cacheKey, {
-      results,
-      yardimKaydi,
-    });
+    await fastifyInstance.cache.set(cacheKey, results, 1000 * 60 * 60);
 
     if (!results) {
       res.statusCode = 404;
