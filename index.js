d531f3bf043932432feac1835c9cd08bb7b20f1d
Okan Pinar <dev.okanpin@gmail.com>
2023-02-12 18:32:30 +0300

    use local cache if redis not available

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
 const fs = require("fs");
 
-if (fs.existsSync(".env")) {
-  require("dotenv").config();
-  console.log("Using environment variables from .env file");
-} else {
-  console.log(".env file not found");
-}
-const config = require("./config.js");
-const buildServer = require("./server/build.js");
+async function main() {
+  if (fs.existsSync(".env")) {
+    require("dotenv").config();
+    console.log("Using environment variables from .env file");
+  } else {
+    console.log(".env file not found");
+  }
+  const config = require("./config.js");
+  const buildServer = require("./server/build.js");
 
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const app = buildServer();
+  const app = await buildServer();
 
-// DB connection
-mongoose
-  .connect(config.mongoUrl, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    app.log.info("Connected to DB...");
-    return app.listen(
-      {
-        port: config.port,
-        host: "0.0.0.0",
-      },
-      (err, address) => {
-        if (config.NODE_ENV === "development") {
-          console.log(`Available routes: \n${app.printRoutes()}`);
-        }
+  // DB connection
+  mongoose
+    .connect(config.mongoUrl, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => {
+      app.log.info("Connected to DB...");
+      return app.listen(
+        {
+          port: config.port,
+          host: "0.0.0.0",
+        },
+        (err, address) => {
+          if (config.NODE_ENV === "development") {
+            console.log(`Available routes: \n${app.printRoutes()}`);
+          }
+        },
+      );
+    })
+    .catch((error) => {
+      if (config.NODE_ENV === "development") {
+        console.log(error);
       }
-    );
-  })
-  .catch((error) => {
-    app.log.fatal("Uncaught error", error);
-    process.exit(0);
-  });
+      app.log.fatal("Uncaught error", error);
+      process.exit(0);
+    });
+}
+
+main();
